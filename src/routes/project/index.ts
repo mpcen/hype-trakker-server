9a1b65a7d7a8f045cd510c7a57c123f7d5534bec
Manny Martinez <emmanuelmartinez.dev@gmail.com>
2022-02-11 02:54:02 -0500

    added 2 routes

--- src/routes/project/index.ts
+++ src/routes/project/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+
 import { createProject } from './create-project';
+import { getProject } from './get-project';
 
 const router = Router();
 
-router.use([createProject]);
+router.use([createProject, getProject]);
 
 export { router as projectRouter };
